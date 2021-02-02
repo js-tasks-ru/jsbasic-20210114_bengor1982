d242e0b87f8df2bf0653da1e59b8b893ef4db029
Bebenin Grigoriy <bengor1982@yandex.ru>
2021-02-02 15:42:15 +0300

    Модуль 3 задачи 1-5 Добавил пятую задачу

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,32 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
+
+//let str = '1, -5.8 ,или, 10, хотя,,,, 34 + -5.3 и 73';
+
 function getMinMax(str) {
-  // ваш код...
+  // Заменим во входной строке все запятые пробелами
+  let spaсeStr = str.replace(/,/g," ");
+  //console.log(spaсeStr);
+  // Разобрать строку в массив по пробелам
+  let massStr = spaсeStr.split(' ');
+  //console.log(massStr);
+  //массив привести к числовому виду 
+  // Ну хз, я привожу все лишнее к НАН
+  let numberStr = massStr.map(item => item !== '' ? item : item - NaN);
+  //console.log(numberStr);
+  //Избавляемся от всего, что дает NAN
+  let cleanNumberStr = numberStr.filter(item => (-Infinity < item && item < Infinity));
+  //console.log(cleanNumberStr);
+  //отобрать максимум и минимум
+  let maxStr = Math.max(...cleanNumberStr);
+  //console.log(maxStr);
+  let minStr = Math.min(...cleanNumberStr);
+  //console.log(minStr);
+  //превратить в обьект с задаными параметрами
+return {min: minStr, max: maxStr,};
+
 }
+//getMinMax(str);
+//console.log(getMinMax(str));

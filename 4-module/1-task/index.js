3595a5337c3f9ac97de0132be43d9df504742c56
Bebenin Grigoriy <bengor1982@yandex.ru>
2021-02-05 19:33:55 +0300

    4 модуль 1-3 задачи

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,6 +3,57 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+/*
+let friends = [
+    {
+        firstName: 'Artsiom',
+        lastName: 'Mezin'
+    },
+    {
+        firstName: 'Ilia',
+        lastName: 'Kantor'
+    },
+    {
+        firstName: 'Christopher',
+        lastName: 'Michael'
+    }
+  ];
+  /*
 function makeFriendsList(friends) {
-  // ваш код...
+   
+//Исходный массив для удобства превратим в массив готовых имен
+
+let friendsMapped = friends.map(friends => (`${friends.firstName} ${friends.lastName}`));
+console.log(friendsMapped)
+//Вводим переменную Html
+let html = '<ul>';
+//Перебором добавляем в html теги с текстом
+friendsMapped.forEach(function(item, i, arr) {
+  html += '<li>'+ item+'</li>';
+});
+html += '</ul>';
+console.log(html)
+return html;
+};
+makeFriendsList(friends)
+
+*/
+function makeFriendsList(friends) {
+  //Исходный массив для удобства превратим в массив готовых имен
+  
+  let friendsMapped = friends.map(friends => (`${friends.firstName} ${friends.lastName}`));
+  //console.log(friendsMapped)
+
+  //Вводим переменную
+  let list = document.createElement("ul");
+  //Перебором добавляем в list теги с текстом
+  for (let i = 0; i < friendsMapped.length; i++) {
+    let newItem = document.createElement("li");
+    let newText = document.createTextNode(friendsMapped[i]);
+    newItem.appendChild(newText);
+    list.appendChild(newItem);
+  }
+  // console.log(list)
+  return list;
 }
+//makeFriendsList(friends)
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,5 +2,42 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
+
 function highlight(table) {
+  //проверяем 3 ячейку каждой строки на наличие атрибута data-available
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let k = 3;
+    if (row.cells[k].hasAttribute('data-available')) {
+      if (row.cells[k].getAttribute('data-available') === 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.setAttribute('hidden', '');
+    }}
+    
+  //проверяем s m и добавляем стиль
+  for (let n = 1; n < table.rows.length; n++) {
+    let ron = table.rows[n];
+    let m = 2;
+    if (ron.cells[m].innerHTML === 'm') {
+        ron.classList.add('male');
+      //ron.cells[m].classList.add('male');
+    } else {
+        ron.classList.add('female');
+      //ron.cells[m].classList.add('female');
+    }}
+
+  //проверяем 1 ячейку каждой строки, сравниваем с 18
+  for (let n = 1; n < table.rows.length; n++) {
+    let ron = table.rows[n];
+    let m = 1;
+    
+    if (ron.cells[m].innerHTML < 18) {
+        ron.style = "text-decoration: line-through";
+      //ron.cells[m].style = "text-decoration: line-through";
+    }}
+
 }
